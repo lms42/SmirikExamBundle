3f7e683313faf6c24a29cb1921cf5cae681a9ffb
Evgeny Smirnov <smirik@gmail.com>
2013-08-05 17:01:16 +0400

    Fix incorrect service declaration.

--- Manager/ExamUserManager.php
+++ Manager/ExamUserManager.php
@@ -6,21 +6,22 @@ class ExamUserManager
 {
     
     protected $question_manager;
+    protected $quiz_question_manager;
     
-    public function setManagers($question_manager)
+    public function setManagers($quiz_question_manager)
     {
-        $this->question_manager = $question_manager;
+        $this->quiz_question_manager = $quiz_question_manager;
     }
     
     public function diff($exam_user)
     {
         $user_quiz = $exam_user->getUserQuiz();
         
-        $questions      = $this->question_manager->getByIds(json_decode($user_quiz->getQuestions()));
-        $user_questions = $this->question_manager->getUserQuestions($user_quiz);
-        $answers        = $this->question_manager->getRightAnswers($questions);
+        $questions      = $this->quiz_question_manager->getByIds(json_decode($user_quiz->getQuestions()));
+        $user_questions = $this->quiz_question_manager->getUserQuestions($user_quiz);
+        $answers        = $this->quiz_question_manager->getRightAnswers($questions);
         
-        $user_answers   = $this->question_manager->getUserAnswers($user_questions);
+        $user_answers   = $this->quiz_question_manager->getUserAnswers($user_questions);
         
         $user_questions = $user_questions->toArray('QuestionId');
         
